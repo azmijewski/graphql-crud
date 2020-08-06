5688cfca83cbd5528f24b15a062d9bcedfd26149
adam.zmijewski <adam.zmijewski@bluesoft.com>
2020-08-06 21:44:48 +0200

    Initial commit

---
+++ src/main/java/com/zmijewski/graphqlcrudexample/GraphqlCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.zmijewski.graphqlcrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlCrudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlCrudExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zmijewski/graphqlcrudexample/GraphqlCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmijewski.graphqlcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
